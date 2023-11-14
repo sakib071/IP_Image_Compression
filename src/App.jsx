6a665b91124ddc0a85cdff7d1b7620535957a878
sakib071 <c193055@ugrad.iiuc.ac.bd>
2023-11-14 21:00:45 +0600

    card added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from 'react';
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
+  // const [items, setItems] = useState();
+  const [algo, setAlgo] = useState([]);
 
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(data => {
+        setAlgo(data)
+        console.log(data);
+      })
+  }, [])
   return (
-    <>
-      <h3>Hi there</h3>
-    </>
+    <div className='max-w-6xl flex flex-col justify-center mt-10 mx-auto'>
+      <h3 className='text-3xl font-bold uppercase mb-5'>Image Compression</h3>
+
+      <div className='grid grid-cols-3 gap-5 mx-auto'>
+        {
+          algo.map(item => <Card key={item.id} item={item}></Card>)
+        }
+      </div>
+    </div>
+
+
   )
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,16 @@
 
-const Card = () => {
-    return (
-        <div>
+const Card = ({ item }) => {
+    const { name, description } = item;
 
+    return (
+        <div className="w-74 h-60 bg-base-100 shadow">
+            <div className="card-body">
+                <h2 className="text-lg font-semibold uppercase">{name}</h2>
+                <p className="text-sm h-28">{description}</p>
+                <button className="btn btn-sm btn-neutral"> Select</button>
+            </div>
         </div>
+
     );
 };
 
